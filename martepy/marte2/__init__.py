036bf520689b55e5d54b6aec469e3cfae1b89ab2
Martino Ferrari <manda.mgf@gmail.com>
2025-01-27 08:56:00 +0100

    fix: missing MARTe2Application in import

--- martepy/marte2/__init__.py
+++ martepy/marte2/__init__.py
@@ -4,6 +4,7 @@ from martepy.marte2.gam import MARTe2GAM
 from martepy.marte2.datasources import *
 from martepy.marte2.gams import *
 from martepy.marte2.objects import *
+from martepy.marte2.generic_application import MARTe2Application
 
 __all__ = [
     "MARTe2GAM",
