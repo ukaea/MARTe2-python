943c9ac0de48dee1cf724fff68339416f2b19fbf
sudilav <85935040+sudilav@users.noreply.github.com>
2025-09-10 16:00:43 +0100

    Update __init__.py

--- martepy/marte2/__init__.py
+++ martepy/marte2/__init__.py
@@ -4,7 +4,6 @@ from martepy.marte2.gam import MARTe2GAM
 from martepy.marte2.datasources import *
 from martepy.marte2.gams import *
 from martepy.marte2.objects import *
-from martepy.marte2.generic_application import MARTe2Application
 
 __all__ = [
     "MARTe2GAM",
